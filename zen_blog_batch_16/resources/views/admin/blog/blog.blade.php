372055733c9e83b7b24e4d9d520e03a910f8c672
mohammadali63 <taheralinur@gmail.com>
2022-12-07 00:55:24 +0600

    Manage Blog page data comin to database dynamicaly

--- zen_blog_batch_16/resources/views/admin/blog/blog.blade.php
+++ zen_blog_batch_16/resources/views/admin/blog/blog.blade.php
@@ -93,37 +93,37 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-xl-9 mx-auto">
-            <hr/>
-            <div class="card">
-                <div class="card-body">
-                    <table class="table table-striped table-bordered table-hover">
-                        <tr>
-                            <th>SL</th>
-                            <th>Author Name</th>
-                            <th>Action</th>
-                        </tr>
-                        @php $i=1 @endphp
-                        @foreach($authors as $author)
-                            <tr>
-                                <td>{{$i++}}</td>
-                                <td>{{$author->author_name}}</td>
-                                <td class="d-flex">
-                                    <a href="{{route('edit.author',['id'=>$author->id])}}" class="btn btn-outline-primary"><i class="bi bi-pencil-fill"></i></a>
+{{--    <div class="row">--}}
+{{--        <div class="col-xl-9 mx-auto">--}}
+{{--            <hr/>--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-body">--}}
+{{--                    <table class="table table-striped table-bordered table-hover">--}}
+{{--                        <tr>--}}
+{{--                            <th>SL</th>--}}
+{{--                            <th>Author Name</th>--}}
+{{--                            <th>Action</th>--}}
+{{--                        </tr>--}}
+{{--                        @php $i=1 @endphp--}}
+{{--                        @foreach($authors as $author)--}}
+{{--                            <tr>--}}
+{{--                                <td>{{$i++}}</td>--}}
+{{--                                <td>{{$author->author_name}}</td>--}}
+{{--                                <td class="d-flex">--}}
+{{--                                    <a href="{{route('edit.author',['id'=>$author->id])}}" class="btn btn-outline-primary"><i class="bi bi-pencil-fill"></i></a>--}}
 
-                                    <form action="{{route('delete.author')}}" method="post">
-                                        @csrf
-                                        <input type="hidden" name="namedelete_id" value="{{$author->id}}">
-                                        <button type="submit" onclick="return confirm('Are you sure delete this.......')" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
-                                    </form>
-                                </td>
+{{--                                    <form action="{{route('delete.author')}}" method="post">--}}
+{{--                                        @csrf--}}
+{{--                                        <input type="hidden" name="namedelete_id" value="{{$author->id}}">--}}
+{{--                                        <button type="submit" onclick="return confirm('Are you sure delete this.......')" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>--}}
+{{--                                    </form>--}}
+{{--                                </td>--}}
 
-                            </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+{{--                            </tr>--}}
+{{--                        @endforeach--}}
+{{--                    </table>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 @endsection
