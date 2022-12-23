02e8c734d70dcba0a1b29530edea01d60bfa44a4
mohammadali63 <taheralinur@gmail.com>
2022-12-23 11:32:16 +0600

    Frontend view mastaring compleate

---
+++ ecommerce_web_b_1/app/Http/Controllers/EcommerceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EcommerceController extends Controller
+{
+    public function index()
+    {
+        return view('frontEnd.home.home');
+    }
+    public function shop()
+    {
+        return view('frontEnd.shop.shop');
+    }
+    public function productDetails()
+    {
+        return value('frontEnd.shop.product-detail');
+    }
+    public function cart()
+    {
+        return view('frontEnd.cart.add-cart');
+    }
+    public function checkout()
+    {
+        return view('frontEnd.checkout.checkout');
+    }
+}
---
+++ ecommerce_web_b_1/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EcommerceController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/',[EcommerceController::class,'index'])->name('home');
+Route::get('/shop',[EcommerceController::class,'shop'])->name('shop');
+Route::get('/single-product',[EcommerceController::class,'productDetails'])->name('single.product');
+Route::get('/cart',[EcommerceController::class,'cart'])->name('cart');
+Route::get('/checkout',[EcommerceController::class,'checkout'])->name('checkout');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+
+//    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+});
