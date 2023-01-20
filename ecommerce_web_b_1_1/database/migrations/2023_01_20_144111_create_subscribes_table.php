2e36c8a449f09339d48e39351bebeb38a1eb4517
mohammadali63 <taheralinur@gmail.com>
2023-01-20 23:31:10 +0600

    Sbuscribe Update User Email is Unique

--- ecommerce_web_b_1_1/database/migrations/2023_01_20_144111_create_subscribes_table.php
+++ ecommerce_web_b_1_1/database/migrations/2023_01_20_144111_create_subscribes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('subscribes', function (Blueprint $table) {
             $table->id();
-            $table->string('subsemail');
+            $table->string('subsemail')->unique();
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
